a8e37b8bddb76c5e29be274de6131e67974795b3
Lada Kesseler <lada.kesseler1@t-mobile.com>
2023-11-10 11:58:10 -0800

    . t using approvals to print empty board

--- src/test/java/org/samples/SampleTests.java
+++ src/test/java/org/samples/SampleTests.java
@@ -6,34 +6,40 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SampleTests
-{
-  @Test
-  public void testNormalJunit()
-  {
-    assertEquals(5, 5);
-  }
-  @Test
-  public void printSimpleBoard()
-  {
- //   Approvals.verify("Hello World");
-    int[][] board = new int[3][3];
-    for (int y = 0; y < 3; y++) {
-      for (int x = 0; x < 3; x++) {
-        System.out.print(board[y][x]);
-      }
-      System.out.println();
+public class SampleTests {
+
+    @Test
+    public void printSimpleBoard() {
+        String result = "";
+        int[][] board = new int[3][3];
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                result += board[y][x];
+            }
+            result += "\n";
+       }
+        Approvals.verify(result);
     }
 
-  }
-  /**
-    *  note: this requires GSON which is currently added in the pom.xml file. 
-    *  This is only required if you want to use the VerifyAsJson.
-    **/
-  @Test
-  public void testJson()
-  {
-    Person hero = new Person("jayne", "cobb", true, 38);
-    Approvals.verifyAsJson(hero);
-  }
+    @Test
+    void whenLifeCellIsInTheMiddleAndItHasNoNeighborTheCellDies() {
+        int[][] board = new int[3][3];
+        board[1][1] = 1;
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                System.out.print(board[y][x]);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * note: this requires GSON which is currently added in the pom.xml file.
+     * This is only required if you want to use the VerifyAsJson.
+     **/
+    @Test
+    public void testJson() {
+        Person hero = new Person("jayne", "cobb", true, 38);
+        Approvals.verifyAsJson(hero);
+    }
 }
