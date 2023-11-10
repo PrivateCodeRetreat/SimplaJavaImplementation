870ff0eed907d5de4bda0722c14f5d4388ab901c
Nazee <nazee@gmail.com>
2023-11-10 12:07:38 -0800

    simple 3x3 board

--- src/test/java/org/samples/SampleTests.java
+++ src/test/java/org/samples/SampleTests.java
@@ -10,29 +10,30 @@ public class SampleTests {
 
     @Test
     public void printSimpleBoard() {
-        String result = "";
         int[][] board = new int[3][3];
-        for (int y = 0; y < 3; y++) {
-            for (int x = 0; x < 3; x++) {
-                result += board[y][x];
-            }
-            result += "\n";
-       }
+        String result = printBoard(board);
         Approvals.verify(result);
     }
+
+
 
     @Test
     void whenLifeCellIsInTheMiddleAndItHasNoNeighborTheCellDies() {
         int[][] board = new int[3][3];
         board[1][1] = 1;
-        for (int y = 0; y < 3; y++) {
-            for (int x = 0; x < 3; x++) {
-                System.out.print(board[y][x]);
-            }
-            System.out.println();
-        }
+       Approvals.verify(printBoard(board));
     }
 
+    private static String printBoard(int[][] board) {
+        String result = "";
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                result += board[y][x];
+            }
+            result += "\n";
+        }
+        return result;
+    }
     /**
      * note: this requires GSON which is currently added in the pom.xml file.
      * This is only required if you want to use the VerifyAsJson.
